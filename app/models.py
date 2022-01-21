dec5fc5193c1eea1d932ca0e6ac61595d1bcaf40
Ahmed-moringa <ahmed.mohamed@student.moringaschool.com>
2022-01-21 08:18:51 +0300

    removed unused commands

--- app/models.py
+++ app/models.py
@@ -4,6 +4,7 @@ from flask_login import UserMixin, current_user
 from werkzeug.security import generate_password_hash, check_password_hash
 from datetime import datetime
 
+
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
@@ -15,13 +16,11 @@ class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(255))
     pass_secure = db.Column(db.String(255))
-    email = db.Column(db.String(255), unique=True, index=True) 
-    #profile_pic_path = db.Column(db.String())
+    email = db.Column(db.String(255), unique=True, index=True)
     pitch = db.relationship('Pitch', backref='user', lazy='dynamic')
     comment = db.relationship('Comment', backref='user', lazy='dynamic')
     upvotes = db.relationship('Upvote', backref='user', lazy='dynamic')
     downvotes = db.relationship('Downvote', backref='user', lazy='dynamic')
-    #posted = db.relationship('Posted', backref='user', lazy='dynamic')
 
     @property
     def password(self):
@@ -49,7 +48,6 @@ class Pitch(db.Model):
     comments = db.relationship('Comment', backref='pitch', lazy='dynamic')
     upvotes = db.relationship('Upvote', backref='pitch', lazy='dynamic')
     downvotes = db.relationship('Downvote', backref='pitch', lazy='dynamic')
-    #posted = db.Column(db.DateTime,default=datetime.utcnow)
 
     @classmethod
     def get_pitches(cls, id):
@@ -64,7 +62,8 @@ class Comment(db.Model):
     __tablename__ = 'comments'
 
     id = db.Column(db.Integer, primary_key=True)
-    pitch_id = db.Column(db.Integer, db.ForeignKey('pitches.id'), nullable=False)
+    pitch_id = db.Column(db.Integer, db.ForeignKey(
+        'pitches.id'), nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
     description = db.Column(db.Text)
 
